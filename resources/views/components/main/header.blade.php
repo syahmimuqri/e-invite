f04552c237010b7e486f6df531c9c08b2b58d901
Rannagi <rannagi99@gmail.com>
2023-10-01 11:44:41 +0800

    Contact Page

--- resources/views/components/main/header.blade.php
+++ resources/views/components/main/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar main-nav navbar-expand-lg px-2 px-sm-0 py-2 py-lg-0">
     <div class="container">
-      <a class="navbar-brand" href="index.html"><img src="{{ asset('main/images/logo.png') }}" alt="logo"></a>
+      <a class="navbar-brand" href="{{ url('') }}/index"><img src="{{ asset('main/images/logo.png') }}" alt="logo"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="ti-menu"></span>
@@ -8,59 +8,59 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item dropdown active">
-            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Home
+            <a class="nav-link dropdown-toggle" href="{{ url('') }}/#" data-toggle="dropdown">Home
               <span><i class="ti-angle-down"></i></span>
             </a>
             <!-- Dropdown list -->
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item active" href="index.html">Homepage</a></li>
-              <li><a class="dropdown-item" href="homepage-2.html">Homepage 2</a></li>
-              <li><a class="dropdown-item active3" href="homepage-3.html">Homepage 3</a></li>
+              <li><a class="dropdown-item active" href="{{ url('') }}/">Homepage</a></li>
+              <li><a class="dropdown-item" href="{{ url('') }}/homepage-2">Homepage 2</a></li>
+              <li><a class="dropdown-item active3" href="{{ url('') }}/homepage-3">Homepage 3</a></li>
   
               <li class="dropdown dropdown-submenu dropright">
-                <a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0301" role="button"
+                <a class="dropdown-item dropdown-toggle" href="{{ url('') }}/#!" id="dropdown0301" role="button"
                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</a>
   
                 <ul class="dropdown-menu" aria-labelledby="dropdown0301">
-                  <li><a class="dropdown-item" href="index.html">Submenu 11</a></li>
-                  <li><a class="dropdown-item" href="index.html">Submenu 12</a></li>
+                  <li><a class="dropdown-item" href="{{ url('') }}/index">Submenu 11</a></li>
+                  <li><a class="dropdown-item" href="{{ url('') }}/index">Submenu 12</a></li>
                 </ul>
               </li>
             </ul>
           </li>
           <li class="nav-item dropdown @@pages">
-            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Pages
+            <a class="nav-link dropdown-toggle" href="{{ url('') }}/#" data-toggle="dropdown">Pages
               <span><i class="ti-angle-down"></i></span>
             </a>
             <!-- Dropdown list -->
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item @@team" href="team.html">Team</a></li>
-              <li><a class="dropdown-item @@career" href="career.html">Career</a></li>
-              <li><a class="dropdown-item @@blog" href="blog.html">Blog</a></li>
-              <li><a class="dropdown-item @@blogSingle" href="blog-single.html">Blog Single</a></li>
-              <li><a class="dropdown-item @@privacy" href="privacy-policy.html">Privacy</a></li>
-              <li><a class="dropdown-item @@faq" href="FAQ.html">FAQ</a></li>
-              <li><a class="dropdown-item" href="sign-in.html">Sign In</a></li>
-              <li><a class="dropdown-item" href="sign-up.html">Sign Up</a></li>
-              <li><a class="dropdown-item" href="404.html">404</a></li>
-              <li><a class="dropdown-item" href="comming-soon.html">Coming Soon</a></li>
+              <li><a class="dropdown-item @@team" href="{{ url('') }}/team">Team</a></li>
+              <li><a class="dropdown-item @@career" href="{{ url('') }}/career">Career</a></li>
+              <li><a class="dropdown-item @@blog" href="{{ url('') }}/blog">Blog</a></li>
+              <li><a class="dropdown-item @@blogSingle" href="{{ url('') }}/blog-single">Blog Single</a></li>
+              <li><a class="dropdown-item @@privacy" href="{{ url('') }}/privacy-policy">Privacy</a></li>
+              <li><a class="dropdown-item @@faq" href="{{ url('') }}/FAQ">FAQ</a></li>
+              <li><a class="dropdown-item" href="{{ url('') }}/sign-in">Sign In</a></li>
+              <li><a class="dropdown-item" href="{{ url('') }}/sign-up">Sign Up</a></li>
+              <li><a class="dropdown-item" href="{{ url('') }}/404">404</a></li>
+              <li><a class="dropdown-item" href="{{ url('') }}/comming-soon">Coming Soon</a></li>
   
               <li class="dropdown dropdown-submenu dropleft">
-                <a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0501" role="button"
+                <a class="dropdown-item dropdown-toggle" href="{{ url('') }}/#!" id="dropdown0501" role="button"
                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</a>
   
                 <ul class="dropdown-menu" aria-labelledby="dropdown0501">
-                  <li><a class="dropdown-item" href="index.html">Submenu 21</a></li>
-                  <li><a class="dropdown-item" href="index.html">Submenu 22</a></li>
+                  <li><a class="dropdown-item" href="{{ url('') }}/index">Submenu 21</a></li>
+                  <li><a class="dropdown-item" href="{{ url('') }}/index">Submenu 22</a></li>
                 </ul>
               </li>
             </ul>
           </li>
           <li class="nav-item @@about">
-            <a class="nav-link" href="about.html">About</a>
+            <a class="nav-link" href="{{ url('') }}/about">About</a>
           </li>
           <li class="nav-item @@contact">
-            <a class="nav-link" href="contact.html">Contact</a>
+            <a class="nav-link" href="{{ url('') }}/contact">Contact</a>
           </li>
         </ul>
       </div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function() { return view('main.index'); });
+Route::get('/contact', function() { return view('main.contact'); });
